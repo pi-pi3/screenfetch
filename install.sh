d22bdf9029a986be78a71c1e6af8260255a1bb43
Szymon Walter <walter.szymon.98@gmail.com>
2017-06-19 12:41:53 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+dir=`realpath $0 | sed 's/[^\/]*$//'`
+
+if [ "$USER" != "root" ]; then
+    echo "This script required root privileges"
+    exit 1
+fi
+
+usage() {
+    echo "Usage: $0 <install|uninstall|deps|help>"
+}
+
+deps() {
+    echo "Installing dependencies"
+    if [[ -x `which pacman` ]]; then
+        pacman -S --needed zsh wmctrl lm_sensors
+    elif [[ -x `which apt-get` ]]; then
+        apt-get install zsh wmctrl lm_sensors
+    elif [[ -x `which aptitude` ]]; then
+        aptitude install zsh wmctrl lm_sensors
+    elif [[ -x `which emerge` ]]; then
+        emerge zsh wmctrl lm_sensors
+    else
+        echo "No default package manager found"
+        echo "Please install the following packages manually and relaunch this script:"
+        echo "zsh wmctrl lm_sensors"
+        exit 1
+    fi
+}
+
+install() {
+    echo "Installing screenfetch"
+    if [[ -x `which screenfetch` ]]; then
+        echo "This program conflicts with \"`which screenfetch`\""
+        echo "Please uninstall it manually and relaunch this script"
+        exit 1
+    fi
+    deps
+    
+    cp $dir/lhs /usr/share/screenfetch
+    cp $dir/screenfetch /bin/screenfetch
+    echo "Successfully installed"
+}
+
+uninstall() {
+    echo "Uninstalling screenfetch"
+    rm /usr/share/screenfetch
+    rm /bin/screenfetch
+    echo "Successfully uninstalled"
+}
+
+case $1 in
+    deps)
+        deps;;
+    install)
+        install;;
+    uninstall)
+        uninstall;;
+    '-h')
+        usage;;
+    '--help')
+        usage;;
+    'help')
+        usage;;
+    *)
+        install;;
+esac
