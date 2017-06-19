e72d3b57f0f555c0dd89aeb7badc908cb11f245d
Szymon Walter <walter.szymon.98@gmail.com>
2017-06-19 14:01:50 +0200

    Add missing dependencies

--- install.sh
+++ install.sh
@@ -13,17 +13,21 @@ usage() {
 deps() {
     echo "Installing dependencies"
     if [[ -x `which pacman` ]]; then
-        pacman -S --needed zsh wmctrl lm_sensors
+        pacman -S --needed zsh wmctrl lm_sensors bc
+        [[ -x `which X` ]] && pacman -S --needed xorg-xprop xorg-xrandr mesa-demos
     elif [[ -x `which apt-get` ]]; then
-        apt-get install zsh wmctrl lm_sensors
+        apt-get install zsh wmctrl lm_sensors bc
+        [[ -x `which X` ]] && apt-get install xorg-xprop xorg-xrandr mesa-demos
     elif [[ -x `which aptitude` ]]; then
-        aptitude install zsh wmctrl lm_sensors
+        aptitude install zsh wmctrl lm_sensors bc
+        [[ -x `which X` ]] && aptitude install xorg-xprop xorg-xrandr mesa-demos
     elif [[ -x `which emerge` ]]; then
-        emerge zsh wmctrl lm_sensors
+        emerge zsh wmctrl lm_sensors bc
+        [[ -x `which X` ]] &&  emerge xorg-xprop xorg-xrandr mesa-demos
     else
-        echo "No default package manager found"
+        echo "No default package manager found (it may simply not be listed)"
         echo "Please install the following packages manually and relaunch this script:"
-        echo "zsh wmctrl lm_sensors"
+        echo "zsh wmctrl lm_sensors bc [xorg-xprop]"
         exit 1
     fi
 }
